ba68dce41528ddb8547c026fbb091e8bea3ca7b8
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2022-06-10 14:21:25 +0300

    faet: сверстать news

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,9 +2,15 @@ import "./NewsCard.css";
 
 export default function MoviesCard({ card }) {
   return (
-    <div className="news-card">
-      <p>{card.content}</p>
-      <img src={card.imageUrl} alt={card.title} />
-    </div>
+    <>
+      <div className="news-card">
+        <img
+          src={card.imageUrl}
+          alt={card.title}
+          className="news-card__image"
+        />
+        <h2 className="news-card__title">{card.content}</h2>
+      </div>
+    </>
   );
 }
